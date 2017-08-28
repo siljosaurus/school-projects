a9c76a95021872e07a30cb611baa301500fb9a41
siljosaurus <31183698+siljosaurus@users.noreply.github.com>
2017-08-28 16:04:42 +0200

    first

---
+++ IN1000 Obligatorisk 1/README.txt
@@ -0,0 +1,12 @@
+a. Hvordan synes du innleveringen var? Hva var enkelt og hva var vanskelig?
+- Jeg synes innleveringen var godt utformet, utfordrende, men ikke umulig.
+Jeg er helt ny til programmering, så jeg merker at jeg bruker litt tid på å tenke struktur,
+men merker at det går bedre og bedre for hver gang jeg skriver et program. Jeg synes også vi har fått veldig
+god introduksjon til programmering gjennom introkurset, forelesningen og med gruppetimene. Så jeg følte jeg stilte
+med litt verktøy i verktøykassen. Jeg brukte mest tid på oppgaven om beslutninger, og minst tid på hei_student og variabler oppgaven.
+
+
+b. Hvor lang tid (ca) brukte du på innleveringen?
+- Brukte til sammen mellom 3-4 timer.
+
+c. Fikk til alle oppgaver :-)
---
+++ IN1000 Obligatorisk 1/variabler.py
@@ -0,0 +1,34 @@
+# I dette programmet har jeg skrevet to ulike program, del 1 adderer heltallsverdier og del 2 skiller mellom to tekststringer.
+
+print("Oppgave 2, del 1:")
+# Siden denne oppgaven består av to deler har jeg skilt dem ved å printe ut en spesifikasjon av oppgave nr.
+
+epler = 4
+appelsiner = 5
+# Her har jeg laget to variabler med heltallsverdier.
+
+print("Epler:", epler)
+print("Appelsiner:", appelsiner)
+# Her bruker jeg print funksjonen for å skrive ut variablene, med påfølgende tekststring for å spesifisere hva tallet står for.
+
+sum = epler + appelsiner
+print("Sum:",sum)
+# Først laget jeg en variabel med navnet "sum", som henter ned og legger sammen de to heltallsverdiene fra variablene "epler" og "appelsiner".
+# Så bruker jeg funksjonen "print" i kombinasjon med en tekststring for å kunne skrive ut summen med forklarende tekst.
+
+print()
+print("Oppgave 2, del 2:")
+# For å skille del 1 og 2 fra hverandre har jeg her også printet en spesifikasjon av oppgave nr, og et blankt linjeskift mellom dem.
+
+ab = "Supercalifragilistic"
+cd = "Expialidocious"
+# Her har jeg laget to variabler med ulike tekststringer.
+
+print(ab)
+print(cd)
+# Her bruker jeg funksjonen print for å synliggjøre tekststringene.
+
+sammen = ab + " " + cd
+print(sammen)
+# Her har jeg laget en variabel for å sette sammen de to tekststringene jeg definerte over, med et mellomrom jeg laget med klammer.
+# Print funksjonen synliggjør de separerte tekststringene.
