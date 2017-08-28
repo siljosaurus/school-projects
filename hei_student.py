b3b0d20ddc5e79f254fcea3a1aaba38df3628fac
siljosaurus <31183698+siljosaurus@users.noreply.github.com>
2017-08-28 16:08:12 +0200

    Delete hei_student.py

--- hei_student.py
+++
@@ -1,9 +0,0 @@
-# Dette programmet gir en personlig hilsen til bruker, ved å bruke input som brukeren selv har lagt inn.
-
-navn = input ("Hei Student! Hva heter du?: ")
-# Her har jeg laget en variabel kalt "navn", som inneholder en tekststring og funksjonen "input".
-# Bruker får spørsmål om hva den heter, og mulighet til å skrive sitt eget navn etter kolon.
-
-print ("Hei,", navn, ":-)")
-# Her bruker jeg funksjonen "print" for å skrive ut tekststringen "Hei". Maskinen henter brukerens input. Jeg la også til en liten tekststring med et smilefjes.
-# Maskinen har interagert med bruker ved å bruke dens input, for å gi en personlig hilsen tilbake.
